02d52d80e81f972389126162fbf2a3a09967c14d
KeremAlpOzecik <keremalpozecik@gmail.com>
2022-03-24 15:51:37 +0300

    Tax table values added with import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.tax(
+    id, category, taxrate)
+VALUES (1, 'FOOD', 1);
+INSERT INTO public.tax(
+    id, category, taxrate)
+VALUES (2, 'STATIONERY',8);
+INSERT INTO public.tax(
+    id, category, taxrate)
+VALUES (3, 'TEXTILE',8);
+INSERT INTO public.tax(
+    id, category, taxrate)
+VALUES (4, 'TECHNOLOGY',18);
+INSERT INTO public.tax(
+    id, category, taxrate)
+VALUES (5, 'CLEANING',18);
+INSERT INTO public.tax(
+    id, category, taxrate)
+VALUES (6, 'OTHERS',18);
+
